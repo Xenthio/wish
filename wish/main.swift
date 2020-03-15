db46cf838f4836ff0f088fa890f45865b0ca1239
Xenthio <thegooster88@gmail.com>
2020-03-16 10:25:29 +1100

    Initial Commit

---
+++ wish/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  wish
+//
+//  Created by Ethan Cardwell on 16/3/20.
+//  Copyright © 2020 Ethan Cardwell. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
